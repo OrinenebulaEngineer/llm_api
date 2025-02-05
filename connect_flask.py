9ad0ed122448a416a08c7653755026a0036a12d8
OrinenebulaEngineer <orinnebula@gmail.com>
2025-02-05 11:18:23 +0330

    commentation

--- connect_flask.py
+++ connect_flask.py
@@ -3,14 +3,14 @@ import paramiko
 
 app = Flask(__name__)
 
-def connect_to_server(server_ip, username, password, command):
+def connect_to_server(server_ip = None, username=None, password=None, command=None):
     try:
         #create an SSH client
         client = paramiko.SSHClient()
         # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        #connect to the server 
-        client.connect(server_ip, username=username, password=password)
+        if server_ip and username and password:
+            #connect to the server 
+            client.connect(server_ip, username=username, password=password)
 
         #Execute a command
         stdin, stdout, stderr = client.exec_command(command=command)
@@ -25,17 +25,18 @@ def connect_to_server(server_ip, username, password, command):
 @app.route('/run_command', methods=['POST'])
 def run_command():
     data = request.json
-    server_ip = data.get("server_ip")
-    username = data.get("username")
-    password = data.get("password")
+    # server_ip = data.get("server_ip")
+    # username = data.get("username")
+    # password = data.get("password")
     command  = data.get("command")
 
     #Check for missing required data
-    if not server_ip or not username or not password:
-        return jsonify({
-            "error" : "Pleas provide remot_ip, username, password"
-        }), 400
-    output, error = connect_to_server(server_ip,username,password,command)
+    # if not server_ip or not username or not password:
+    #     return jsonify({
+    #         "error" : "Pleas provide remot_ip, username, password"
+    #     }), 400
+    # output, error = connect_to_server(server_ip,username,password,command)
+    output, error = connect_to_server(command=command)
 
     if error:
         return jsonify({"error" : error}), 500
