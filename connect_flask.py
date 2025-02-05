aa9ece8f3311516475a72b9e24eb6a6c0b6e0bbf
OrinenebulaEngineer <orinnebula@gmail.com>
2025-02-05 11:32:03 +0330

    uncommentation

--- connect_flask.py
+++ connect_flask.py
@@ -3,11 +3,11 @@ import paramiko
 
 app = Flask(__name__)
 
-def connect_to_server(server_ip = None, username=None, password=None, command=None):
+def connect_to_server(server_ip , username, password, command):
     try:
         #create an SSH client
         client = paramiko.SSHClient()
-        # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         if server_ip and username and password:
             #connect to the server 
             client.connect(server_ip, username=username, password=password)
@@ -25,18 +25,18 @@ def connect_to_server(server_ip = None, username=None, password=None, command=No
 @app.route('/run_command', methods=['POST'])
 def run_command():
     data = request.json
-    # server_ip = data.get("server_ip")
-    # username = data.get("username")
-    # password = data.get("password")
+    server_ip = data.get("server_ip")
+    username = data.get("username")
+    password = data.get("password")
     command  = data.get("command")
 
     #Check for missing required data
-    # if not server_ip or not username or not password:
-    #     return jsonify({
-    #         "error" : "Pleas provide remot_ip, username, password"
-    #     }), 400
-    # output, error = connect_to_server(server_ip,username,password,command)
-    output, error = connect_to_server(command=command)
+    if not server_ip or not username or not password:
+        return jsonify({
+            "error" : "Pleas provide remot_ip, username, password"
+        }), 400
+    output, error = connect_to_server(server_ip,username,password,command)
+    # output, error = connect_to_server(command=command)
 
     if error:
         return jsonify({"error" : error}), 500
@@ -45,4 +45,7 @@ def run_command():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port =5000)
+    
+
+
     
