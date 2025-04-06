9a75beb0893f774598869a39bf1cfdfc681d01a6
OrinenebulaEngineer <orinnebula@gmail.com>
2025-04-06 14:10:18 +0330

    add vllm and ollama

--- llm.py
+++ llm.py
@@ -1,23 +1,15 @@
-from ollama import chat
-from ollama import ChatResponse
+from vllm import LLM, ChatResponse
 
 class Llm:
     def __init__(self):
-        pass
-
-
+        self.model = LLM(model="google/gemma-2-9b-it")  
     def response(self, prompt):
-                
         message = [
-                {"role" : "system" , "content" : "you are helpful assistance provide good answer based on  prompt language"},
-                {"role" : "user",  "content" : prompt}
-            ]
+            {"role": "system", "content": "you are helpful assistance provide good answer based on prompt language"},
+            {"role": "user", "content": prompt}
+        ]
 
-        response : ChatResponse = chat(
-                model = 'qwen:32b',
-                messages= message)
-            
+        response: ChatResponse = self.model.chat(messages=message)
+
         output = response.message.content
-        # print(output)
-
         return output
